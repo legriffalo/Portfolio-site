3cf4ce101aea27ac1e8ca206dec643db03ff5a5e
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2024-10-17 09:02:07 +0300

    updating portfolio data

--- portfolio3.0/js/myprojects.js
+++ portfolio3.0/js/myprojects.js
@@ -4,7 +4,81 @@ const data = `{
 
 "1":{
     "show":"1",
+    "inprogress":0,
+    "name":"Speedia extension",
+    "coverimage":"./assets/projects/speedia.png",
+    "description":"Building a chrome extension",
+    "specifics":["I got tired of being locked at a set media speed on training videos/site",
+                "So I decided to build an extension to help me speed things up.",
+                "Using Manifest 3.0 and web languages I built an extension that allows media playback changes and has a useful speed reader"],
+    "tags":["JS","Web","UI","API"],
+    
+    "link":{"source":"https://github.com/legriffalo/speedmedia-chrome-extension","type":"github"},
+
+    "general":["data"],
+    "otherlinks":[]
+    },
+
+
+"2":{
+    "show":"1",
+    "inprogress":1,
+
+    "name":"Finnish vocabulary quiz",
+    "coverimage":"./assets/projects/vocabgames.png",
+    "description":"The intial MVP phase of larger project",
+    "specifics":["I needed a way to quickly practice and expand my Finnish vocabulary",
+                "To do this I built a small PWA to load on to my phone to use for daily vocab building",
+                "I used my data base of finnish words and a Python script to create JSON flashcards sets.",
+                "These can be loaded into the vocab PWA to be used",
+                "A set of 5000 words will be hosted on a simple sit to make this process simpler"],
+    "tags":["Python","JS","UI","PWA","JSON", "Data","App"],
+    "link":{"source":"https://github.com/legriffalo-alt/flashcards","type":"github"},
+    "general":["data"],
+    "otherlinks":[{"source":"https://legriffalo-alt.github.io/flashcards/flashcards.html","type":"site"}]
+    },
+
+"3":{
+    "show":"1",
+    "inprogress":0,
+
+    "name":"Stop smoking tracker",
+    "coverimage":"./assets/projects/stopsmoking.png",
+    "description":"The intial scraping phase of a larger project",
+    "specifics":["I wnated to quit smoking more effectively",
+                "To do this I wanted an accountability app that monitored/showed my progress",
+                "I had previously struggled to appreciate 3rd party apps so I made my own",
+                "The app is a PWA using manifest that tracks quitting stats and keeps a smoking/quitting log in a calendar using calendar.js"],
+    "tags":["JS","Data","JSON","Web","UI","App"],
+    "link":{"source":"https://github.com/legriffalo/finnishvocab/tree/main/Finnish%20scraping%20project","type":"github"},
+    "general":["data"],
+    "otherlinks":[{"source":"https://legriffalo.github.io/quitting-smoking/stopsmoking.html","type":"site"}]    },
+
+
+"4":{
+    "show":"1",
+    "inprogress":0,
+
     "name":"Finnish webscraper",
+    "coverimage":"./assets/projects/scraping.png",
+    "description":"The intial scraping phase of a larger project",
+    "specifics":["I needed a database of Finnish words on which to base later learning projects.",
+                "To do this I used beautiful soup to crawl finnish grammar websites",
+                "Once scraped,my plan was to load the words into a database to form a backend for subsequent projects"],
+    "tags":["Python","data","scraping","datamining"],
+    "link":{"source":"https://github.com/legriffalo/finnishvocab/tree/main/Finnish%20scraping%20project","type":"github"},
+    "general":["data"],
+    "otherlinks":[]
+    },
+
+
+
+
+"5":{
+    "show":"1",
+    "inprogress":1,
+
+    "name":"",
     "coverimage":"./assets/image.png",
     "description":"The intial scraping phase of a larger project",
     "specifics":["I needed a database of Finnish words on which to base later learning projects.",
@@ -16,8 +90,10 @@ const data = `{
     "otherlinks":[]
     },
 
-"2":{
+"6":{
     "show":"1",
+    "inprogress":1,
+
     "name":"Image storage",
     "coverimage":"./assets/image.png",
     "description":"Webpage to facilitate collecting image for CNN image recognition projects",
@@ -31,78 +107,24 @@ const data = `{
 
     },
 
-"3":{
-    "show":"1",
-    "name":"Vocab database",
-    "coverimage":"./assets/image.png",
-    "description":"Building and managing postgres database for Finnish language projects",
-    "specifics": ["","",""],
-    "tags":["SQL","python","flask","back-end"],
-    "link":{"source":"https://github.com/legriffalo/finnishvocab/tree/main/Database%20management","type":"github"}
-    },
-
-"4":{
-    "show":"1",
-    "name":"Number recognition",
-    "coverimage":"./assets/image.png",
-    "description":"Building a CNN to read digits for use in Finnish language app",
-    "specifics": ["","",""],
-    "tags":["Tensorflow","python","SQL","datascience","machine learning"],
-    "link":{"source":"","type":"github"}
-    },
-
-"5":{
-    "show":"1",
-    "name":"Finnish practice app",
-    "coverimage":"./assets/image.png",
-    "description":"Building a CNN to read digits for use in Finnish language app",
-    "specifics": ["","",""],
-    "tags":["Tensorflow","python","SQL","datascience","machine learning"],
-    "link":{"source":"","type":"github"}
-    },
-
-"6":{
-    "show":"1",
-    "name":"Air quality dashboard",
-    "coverimage":"./assets/image.png",
-    "description":"Building a CNN to read digits for use in Finnish language app",
-    "specifics": ["","",""],
-    "tags":["Tensorflow","python","SQL","datascience","machine learning"],
-    "link":{"source":"","type":"github"}
-    },
-
 "7":{
     "show":"1",
-    "name":"10 week Python course",
-    "coverimage":"./assets/image.png",
-    "description":"Building a CNN to read digits for use in Finnish language app",
-    "specifics": ["","",""],
-    "tags":["Tensorflow","python","SQL","datascience","machine learning"],
-    "link":{"source":"","type":"github"}
-    },
+    "inprogress":1,
+
+    "name":"Molkky madness app",
+    "coverimage":"./assets/molkky.png",
+    "description":"A scoring and custom rule making app for web/mobile",
+    "specifics": ["This was built using javaScript, html, css in the first instance to make a webpage to test the useability",
+                  "Then using githubpages and reacte native webview I published it as a mobile app."],
+    "tags":["html","javaScript","css","react-native","googlePlay", "app","web","front-end"],
+    "link":{"source":"https://legriffalo.github.io/molkkymadness/molkky.html","type":"github"}
+},
+
 
 "8":{
     "show":"1",
-    "name":"10 week Python course",
-    "coverimage":"./assets/image.png",
-    "description":"Building a CNN to read digits for use in Finnish language app",
-    "specifics": ["","",""],
-    "tags":["Tensorflow","python","SQL","datascience","machine learning"],
-    "link":{"source":"","type":"github"}
-    },
+    "inprogress":1,
 
-"9":{
-    "show":"1",
-    "name":"10 week Python course",
-    "coverimage":"./assets/image.png",
-    "description":"Building a CNN to read digits for use in Finnish language app",
-    "specifics": ["","",""],
-    "tags":["Tensorflow","python","SQL","datascience","machine learning"],
-    "link":{"source":"","type":"github"}
-    },    
-
-"10":{
-    "show":"1",
     "name":"Molkky madness app",
     "coverimage":"./assets/molkky.png",
     "description":"A scoring and custom rule making app for web/mobile",
@@ -112,4 +134,100 @@ const data = `{
     "link":{"source":"https://legriffalo.github.io/molkkymadness/molkky.html","type":"github"}
 }
 
+
+
 }`
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "3":{
+//     "show":"1",
+//     "name":"Vocab database",
+//     "coverimage":"./assets/image.png",
+//     "description":"Building and managing postgres database for Finnish language projects",
+//     "specifics": ["","",""],
+//     "tags":["SQL","python","flask","back-end"],
+//     "link":{"source":"https://github.com/legriffalo/finnishvocab/tree/main/Database%20management","type":"github"}
+//     },
+
+// "4":{
+//     "show":"1",
+//     "name":"Number recognition",
+//     "coverimage":"./assets/image.png",
+//     "description":"Building a CNN to read digits for use in Finnish language app",
+//     "specifics": ["","",""],
+//     "tags":["Tensorflow","python","SQL","datascience","machine learning"],
+//     "link":{"source":"","type":"github"}
+//     },
+
+// "5":{
+//     "show":"1",
+//     "name":"Finnish practice app",
+//     "coverimage":"./assets/image.png",
+//     "description":"Building a CNN to read digits for use in Finnish language app",
+//     "specifics": ["","",""],
+//     "tags":["Tensorflow","python","SQL","datascience","machine learning"],
+//     "link":{"source":"","type":"github"}
+//     },
+
+// "6":{
+//     "show":"1",
+//     "name":"Air quality dashboard",
+//     "coverimage":"./assets/image.png",
+//     "description":"Building a CNN to read digits for use in Finnish language app",
+//     "specifics": ["","",""],
+//     "tags":["Tensorflow","python","SQL","datascience","machine learning"],
+//     "link":{"source":"","type":"github"}
+//     },
+
+// "7":{
+//     "show":"1",
+//     "name":"10 week Python course",
+//     "coverimage":"./assets/image.png",
+//     "description":"Building a CNN to read digits for use in Finnish language app",
+//     "specifics": ["","",""],
+//     "tags":["Tensorflow","python","SQL","datascience","machine learning"],
+//     "link":{"source":"","type":"github"}
+//     },
+
+// "8":{
+//     "show":"1",
+//     "name":"10 week Python course",
+//     "coverimage":"./assets/image.png",
+//     "description":"Building a CNN to read digits for use in Finnish language app",
+//     "specifics": ["","",""],
+//     "tags":["Tensorflow","python","SQL","datascience","machine learning"],
+//     "link":{"source":"","type":"github"}
+//     },
+
+// "9":{
+//     "show":"1",
+//     "name":"10 week Python course",
+//     "coverimage":"./assets/image.png",
+//     "description":"Building a CNN to read digits for use in Finnish language app",
+//     "specifics": ["","",""],
+//     "tags":["Tensorflow","python","SQL","datascience","machine learning"],
+//     "link":{"source":"","type":"github"}
+//     },    
+
+// "10":{
+//     "show":"1",
+//     "name":"Molkky madness app",
+//     "coverimage":"./assets/molkky.png",
+//     "description":"A scoring and custom rule making app for web/mobile",
+//     "specifics": ["This was built using javaScript, html, css in the first instance to make a webpage to test the useability",
+//                   "Then using githubpages and reacte native webview I published it as a mobile app."],
+//     "tags":["html","javaScript","css","react-native","googlePlay", "app","web","front-end"],
+//     "link":{"source":"https://legriffalo.github.io/molkkymadness/molkky.html","type":"github"}
+// }
+
